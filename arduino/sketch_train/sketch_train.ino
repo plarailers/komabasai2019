3e7415cfb0ed074073847ad9e61d7e9c681dbc72
Yasuyuki Taguchi <tagyasu@g.ecc.u-tokyo.ac.jp>
2019-11-14 00:28:58 +0900

    テスト

--- arduino/sketch_train/sketch_train.ino
+++ arduino/sketch_train/sketch_train.ino
@@ -11,6 +11,8 @@ const int outPinB = 9;  //モーターのIN2
 const int recvPin = 5; //赤外線の信号を受信
 const int sensorPin = A6; //CdSセンサーの計測
 const int volt_sensorPin = A7;  //電圧の計測
+int CDS = []
+
 
 int value;  //CdSセンサーの計測値を格納
 double volt_value;  //電圧の計測値を格納
@@ -88,51 +90,51 @@ void move(decode_results *results, byte speed) {  //信号を受け取って動�
 void loop() {
   Serial.print(flagBefore);
   Serial.print(flagNow);
-  
+
   decode_results  results;  //赤外線の指令を受け取る
-  
+
   volt_value = analogRead(volt_sensorPin);  //電圧を計測
   volt_value *= 10;  //電圧をV単位に変換
   volt_value /= 1024;
   Serial.print(" ");
   Serial.print(volt_value); //変換後の電圧値を表示
   Serial.print("V");
-  
+
   if (volt_value < minVolt) { //電圧が最低電圧よりも低かったら
     Serial.print(" battery_shortage");
     irsend.sendNEC(channel_5, 32);  //電池が少ないことを母艦に伝える
     irrecv.enableIRIn();
     speed = 255;  //最大の電圧をモーターにかける
     }
-    
+
   else if (volt_value >= minVolt) { //電池が十分にあれば
     speed = 255*(minVolt/volt_value); //電圧が高い時ほどモーターに電圧をかけないようにして速度を一定にする
   }
-  
+
   Serial.print(" speed:");  //スピードを表示
   Serial.print(speed); //電圧値によって決定したspeedを表示
   Serial.print(" ");
-  
+
   if (irrecv.decode(&results)) {  //赤外線の指令を受け取ってモーターを動かす
     printNumber(&results);  //受け取った信号を表示
     move(&results, speed); //受け取った信号に応じた動きをする
     irrecv.resume();  //また信号を受け取れるようにする
   }
-  
+
   value = analogRead(sensorPin);  //CdSセンサーで明るさを計測
   Serial.print(" light:");
   Serial.println(value);  //読み取った明るさを表示
-  
+
   if (value > maxCdS) {  //明るさが大きければ
     Serial.print(" marker_exist"); //マーカーがあった
 //    irsend.sendNEC(channel_4, 32);  //母艦にマーカーの存在を伝達
 //    irrecv.enableIRIn();  //また赤外線の信号を受け取れるようにする
     if (flagBefore == flagNow) { //赤外線による信号が
       //直前のものと同じなら
-      stop();  
+      stop();
     }
   else {//違うならそのまま止める。
-    
+
     delay(2000);//時間をちょっと空けておく
     flagBefore = flagNow;
     }
