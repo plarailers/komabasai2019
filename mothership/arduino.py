bbe50a4bd24f4001fb8e42b76256e650a69cad82
n4o847 <yuu.miu.0925@gmail.com>
2019-11-13 20:41:35 +0900

    Fix

--- mothership/arduino.py
+++ mothership/arduino.py
@@ -21,17 +21,18 @@ def communicate(recv_queue, send_queue):
 
     # 通信開始
     with serial.Serial(port, baudrate=9600) as ser:
-        # 受信バッファにデータがあるなら
-        while ser.in_waiting >= 4:
-            # データを4バイト読み込み、整数に変換。受信キューに入れる。
-            data_int = ser.read(4)
-            data_bytes = int.from_bytes(data_int, byteorder="big")
-            recv_queue.put(data_bytes)
-        # 送信キューにデータがあるなら
-        while not send_queue.empty():
-            # データを読み込み、バイト列に変換。送信する。
-            data_int = send_queue.get()
-            data_bytes = data_int.to_bytes(4, byteorder='big')
-            ser.write(data_bytes)
-        # 1秒待つ
-        time.sleep(1)
+        while True:
+            # 受信バッファにデータがあるなら
+            while ser.in_waiting >= 4:
+                # データを4バイト読み込み、整数に変換。受信キューに入れる。
+                data_bytes = ser.read(4)
+                data_int = int.from_bytes(data_bytes, byteorder="big")
+                recv_queue.put(data_int)
+            # 送信キューにデータがあるなら
+            while not send_queue.empty():
+                # データを取り出し、バイト列に変換。送信する。
+                data_int = send_queue.get()
+                data_bytes = data_int.to_bytes(4, byteorder="big")
+                ser.write(data_bytes)
+            # 1秒待つ
+            time.sleep(1)
