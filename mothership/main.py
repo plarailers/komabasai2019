ec1c3d3fd84057d8c5b6b72f47223f76829ca853
n4o847 <yuu.miu.0925@gmail.com>
2019-11-17 11:55:50 +0900

    スレッド廃止

--- mothership/main.py
+++ mothership/main.py
@@ -1,11 +1,47 @@
-import threading
+import serial
+import serial.tools.list_ports
+import time
 from queue import Queue
-import arduino
+
+arduino = None
+recv_queue = Queue()
+send_queue = Queue()
+
+def setup():
+    port_list = serial.tools.list_ports.comports()
+    if len(port_list) == 0:
+        print("ポートがありません。")
+        exit()
+    print("ポートを選択:")
+    for port in port_list:
+        print("-", port)
+    port_name = input()
+    arduino = serial.Serial(port_name, baudrate=9600)
+    print("通信開始")
+
+
+def loop():
+    # 受信バッファにデータがあるなら
+    while arduino.in_waiting >= 4:
+        # データを4バイト読み込み、整数に変換。受信キューに入れる。
+        data_bytes = arduino.read(4)
+        data_int = int.from_bytes(data_bytes, byteorder="big")
+        recv_queue.put(data_int)
+        print("受信:", "0x{:08X}".format(data_int))
+
+    send_queue.put(0x20DFDB8C)
+
+    # 送信キューにデータがあるなら
+    while not send_queue.empty():
+        # データを取り出し、バイト列に変換。送信する。
+        data_int = send_queue.get()
+        data_bytes = data_int.to_bytes(4, byteorder="big")
+        arduino.write(data_bytes)
+        print("送信:", "0x{:08X}".format(data_int))
+
 
 if __name__ == "__main__":
-    recv_queue = Queue()
-    send_queue = Queue()
-    threading.Thread(
-        target=arduino.communicate,
-        kwargs={"recv_queue": recv_queue, "send_queue": send_queue}
-    ).start()
+    setup()
+    while True:
+        loop()
+        time.sleep(1)
