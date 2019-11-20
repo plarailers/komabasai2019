a5a1fd1da8b95067f293b3097c04271cf0dc753f
tagyasu <42203711+tagyasu@users.noreply.github.com>
2019-11-20 23:49:43 +0900

    Update sketch_staiton.ino

--- arduino/sketch_staiton.ino
+++ arduino/sketch_staiton.ino
@@ -4,11 +4,18 @@
 VarSpeedServo servo1;
 VarSpeedServo servo2;
 
-int servoSpeed = /*サーボのスピード*/; //1から255、10から30程度が最適か
-int servo1_RightAngle = ;//サーボ1を右にふるときの角度
-int servo1_LeftAngle = ;//0から180
-int servo2_RightAngle = ;
-int servo2_LeftAngle = ;
+const int servoSpeed = /*サーボのスピード*/; //1から255、10から30程度が最適か
+const int servo1_RightAngle = ;//サーボ1を右にふるときの角度
+const int servo1_LeftAngle = ;//0から180
+const int servo2_RightAngle = ;
+const int servo2_LeftAngle = ;
+
+
+const int outPin = ;//赤外線の送信ピン
+const int recvPin = ;//赤外線の受信ピン
+int data = 0;//受信データ格納用
+IRrecv irrecv(recvPin);
+decode_results results;//うまくいかないときはloopの中へ
 
 void servo1Right(){//サーボを右にふる関数
   servo1.write(servo1_RightAngle, servoSpeed, true);
@@ -23,14 +30,23 @@ void servo2Left(){
   servo2.write(servo2_LefttAngle, servoSpeed, true);
   }
 
-
-
-
 void setup(){
   Serial.begin(9600);
   irrecv.enableIRIn();
   servo1.attach(/*ピン番号*/);
   servo2.attach(/*ピン番号*/);
+  pinMode(outPin, OUTPUT);
   }
 
+void loop(){
+  if(irrecv.decode(&results)){//車両から受け取った信号をシリアルで送信
+    data = decode.value % 256;
+    decode.resume();
+    Serial.write(data);//megaの場合はserial1を使う
+    }
+  
+  
+    
+    
+  
   
