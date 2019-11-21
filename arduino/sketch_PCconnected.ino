d09b316354f236a1d4032402e4038af25eb28dec
tagyasu <42203711+tagyasu@users.noreply.github.com>
2019-11-21 17:11:12 +0900

    Update and rename sketch_PCconnected to sketch_PCconnected.ino

--- arduino/sketch_PCconnected.ino
+++ arduino/sketch_PCconnected.ino
@@ -1,4 +1,7 @@
 //Serial1は東京、Serial2は小田原、Serial3は静岡、SoftwareSerialは名古屋
+
+int data = 0;//データ格納用
+
 void setup(){
   Serial.begin(9600);
   Serial1.begin(9600);
@@ -7,4 +10,14 @@ void setup(){
   SoftwareSerial(10,11);
   SoftwareSerial.begin(9600);
   }
+void loop(){
+  
+  while(Serial1.available() > 0){
+   data = Serial1.read();
+   Serial.write(data);
+   }
+   
+   
+    
+    
  
