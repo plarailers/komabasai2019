4ecd088aec1aca4670a77c1e58ecfa958b5ca792
tagyasu <42203711+tagyasu@users.noreply.github.com>
2019-11-22 10:46:09 +0900

    Update shizuoka.ino

--- arduino/sketch_staion/shizuoka.ino
+++ arduino/sketch_staion/shizuoka.ino
@@ -6,10 +6,10 @@
 VarSpeedServo servo1;
 VarSpeedServo servo2;
 
-const unsigned long servo1_Siganl0 = 0x20DFDBE8;//サーボを動かす信号
-const unsigned long servo1_Siganl1 = 0x20DFDB18;
-const unsigned long servo2_Siganl0 = 0x20DFDB98;
-const unsigned long servo2_Siganl1 = 0x20DFDB58;
+const unsigned long servo1_Siganl0 = 0xE8;//サーボを動かす信号
+const unsigned long servo1_Siganl1 = 0x18;
+const unsigned long servo2_Siganl0 = 0x98;
+const unsigned long servo2_Siganl1 = 0x58;
 
 
 const int servoSpeed = 50; //1から255
@@ -52,8 +52,10 @@ void loop(){
   if(irrecv.decode(&results)){//車両から受け取った信号をシリアルで送信
     data = results.value % 256;//4バイト→1バイト
     irrecv.resume();
-    Serial.write(data);//megaの場合はserial1を使う
+    if(data == 0x2C ||data == 0x20 ||data == 0x28 ||data == 0x24 ||data == 0x5C ||data == 0xAC ||data == 0xA0 ||data == 0xA8 ||data == 0xA4 ||data == 0xDC){
+    Serial1.write(data);//megaの場合はserial1を使う
     }
+   }
 
   while(Serial1.available() > 0){//シリアルで受け取った信号をもとに車両に送信またはサーボを動かす
     data = Serial1.read();  
